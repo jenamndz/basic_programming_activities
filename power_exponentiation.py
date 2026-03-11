88655bcff4357360a6f09f6cb4f3cf136d43524b
jenamndz <jenamndz18@gmail.com>
2026-03-11 22:05:34 +0800

    prints the result

--- power_exponentiation.py
+++ power_exponentiation.py
@@ -1,2 +1,5 @@
 first_number = int(input("Enter the base number: "))
 second_number = int(input("Enter the power/exponent: "))
+
+result = first_number ** second_number
+print("The result is:", result)
