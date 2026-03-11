ae313630e523d7b7e93884de5544f7a1c6d7d200
jenamndz <jenamndz18@gmail.com>
2026-03-11 21:30:16 +0800

    shows the higher number between the two inputed number

--- high_number_identifier.py
+++ high_number_identifier.py
@@ -1,2 +1,8 @@
 first_number = int(input("Enter a number: "))
 second_number = int(input("Enter another number: "))
+
+if first_number > second_number:
+    print(f"The first number is greater than the second number: {first_number}")
+
+else:
+    print(f"The second number is greater than the first number: {second_number}")
