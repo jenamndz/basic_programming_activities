cff38b78aee396b9b826180e4866f8edc4899b1e
jenamndz <jenamndz18@gmail.com>
2026-03-11 21:52:29 +0800

    shows the quotient of the two given numbers

--- divide_two_numbers.py
+++ divide_two_numbers.py
@@ -1,2 +1,5 @@
 first_number = int(input("Enter the first number: "))
 second_number = int(input("Enter the second number: "))
+
+quotient = first_number // second_number
+print("The quotient of", first_number, "and", second_number, "is:", quotient)
