da0afce2bb06bf56b2ecc1d06ad2dc426b73fb80
jenamndz <jenamndz18@gmail.com>
2026-03-11 21:23:40 +0800

    prints the sum of two numbers

--- sum_two_numbers.py
+++ sum_two_numbers.py
@@ -1,2 +1,5 @@
 first_number = float(input("Enter the first number: "))
 second_number = float(input("Enter the second number: "))
+
+first_number += second_number
+print("The sum of the two numbers is:", first_number)
