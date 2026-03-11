b9095dcd4b2197f8a8137d3789a4f88507cde2c5
jenamndz <jenamndz18@gmail.com>
2026-03-11 22:31:09 +0800

    shows the answer or product of the given numbers

--- product_of_two_numbers.py
+++ product_of_two_numbers.py
@@ -1,2 +1,5 @@
 first_number = int(input("Enter the first number: "))
 second_number = int(input("Enter the second number: "))
+
+product = first_number * second_number
+print("The product of the two numbers is:", product)
