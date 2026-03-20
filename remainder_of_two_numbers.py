a903288309e2a420b5bd8143f3c226d1b17b1fb4
jenamndz <jenamndz18@gmail.com>
2026-03-20 16:26:03 +0800

    prints the remainder of the two numbers

--- remainder_of_two_numbers.py
+++ remainder_of_two_numbers.py
@@ -1,2 +1,5 @@
 first_number = int(input("Enter the first number: "))
 second_number = int(input("Enter the second number: "))
+
+remainder = first_number % second_number
+print("The remainder of", first_number, "and", second_number, "is", remainder)
