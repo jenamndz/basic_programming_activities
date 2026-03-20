2b62bea88db0c85efcd0a26559dba6eaaca0b2ee
jenamndz <jenamndz18@gmail.com>
2026-03-20 17:38:19 +0800

    subtracts the following numbers to the first entered number and prints it

--- subtract_sequence_from_first.py
+++ subtract_sequence_from_first.py
@@ -3,3 +3,10 @@ numbers = []
 for i in range(10):
     num = float(input(f"Enter number {i+1}: "))
     numbers.append(num)
+
+answer = numbers[0]
+
+for i in range(1,10):
+    answer -= numbers[i]
+
+print(f"\nThe answer is: {answer}")
