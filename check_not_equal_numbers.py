647840d95aeadce51cc98ba1c7eded723b193428
jenamndz <jenamndz18@gmail.com>
2026-03-20 16:11:02 +0800

    checks and prints if the 2 numbers are not equal or equal

--- check_not_equal_numbers.py
+++ check_not_equal_numbers.py
@@ -1,2 +1,8 @@
 first_number = int(input("Enter the first number: "))
 second_number = int(input("Enter the second number: "))
+
+if first_number != second_number:
+    print("The numbers are not equal")
+
+else:
+    print("The numbers are equal")
