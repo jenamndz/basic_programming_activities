5fda36dff4028374b09fb867467ef8cc921ed909
jenamndz <jenamndz18@gmail.com>
2026-03-20 17:46:35 +0800

    prints the number of even numbers

--- count_even_numbers.py
+++ count_even_numbers.py
@@ -2,5 +2,7 @@ even = 0
 
 for i in range(1, 11):
     number = int(input(f"Enter number {i}: "))
-    if i % 2 == 0:
-        even += i
+    if number % 2 == 0:
+        even += 1
+
+print(f"There are {even} even numbers")
