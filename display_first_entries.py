565859b0a2e242945eb00249cc010b2628a47fd6
jenamndz <jenamndz18@gmail.com>
2026-04-08 18:46:31 +0800

    changed the float into int and prints the first entry numbers

--- display_first_entries.py
+++ display_first_entries.py
@@ -1,5 +1,13 @@
 numbers = []
 
 for i in range (10):
-    num = float(input(f"Enter number {i+1}: "))
+    num = int(input(f"Enter number {i+1}: "))
     numbers.append(num)
+
+seen = []
+
+print("Numbers (duplicates shown only once):")
+for num in numbers:
+    if num not in seen:
+        print(num)
+        seen.append(num)
