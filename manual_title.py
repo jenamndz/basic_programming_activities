054c0d88c06135ebc44ae627e614850f3e51f5a7
jenamndz <jenamndz18@gmail.com>
2026-04-09 04:23:33 +0800

    check if it is the first character or the last character is a space

--- manual_title.py
+++ manual_title.py
@@ -1,2 +1,8 @@
 text = input("Enter string: ")
 result = ""
+
+for i in range(len(text)):
+    if i == 0 or text[i-1] == " ":
+        result += text[i].upper()
+    else:
+        result += text[i].lower()
