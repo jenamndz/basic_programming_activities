e05139c8fd39c0ddfae5aedd76634bc934b7b5a8
jenamndz <jenamndz18@gmail.com>
2026-04-08 23:49:48 +0800

    prints the mode or the most frequent entered number and if there is no number is entered

--- find_most_frequent_number.py
+++ find_most_frequent_number.py
@@ -15,3 +15,9 @@ if numbers:
     most_frequent = max(set(numbers), key=numbers.count)
 
     occurrence = numbers.count(most_frequent)
+
+
+    print(f"\nThe number with the most duplicates is: {most_frequent}")
+    print(f"It appeared {occurrence} times.")
+else:
+    print("No numbers were entered.")
