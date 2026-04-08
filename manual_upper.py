7119f6223ea53ca84ef986985afc7ffbaa3290ae
jenamndz <jenamndz18@gmail.com>
2026-04-09 04:49:13 +0800

    finds the characters from the entered text in the lower list and pull the same letter from the upper list, and if the character is already in uppercase it will stay the same

--- manual_upper.py
+++ manual_upper.py
@@ -3,3 +3,10 @@ text = input("Enter string: ")
 UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 lower = "abcdefghijklmnopqrstuvwxyz"
 result = ""
+
+for char in text:
+    if char in lower:
+        index = lower.find(char)
+        result += UPPER[index]
+    else:
+        result += char
