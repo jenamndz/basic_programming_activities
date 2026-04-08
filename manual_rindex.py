4f20794b3677404c5c2a9ad5cfff24d1644da239
jenamndz <jenamndz18@gmail.com>
2026-04-09 05:30:49 +0800

    looks for the target character starting from the end and stops when found

--- manual_rindex.py
+++ manual_rindex.py
@@ -1,2 +1,9 @@
 text = input("Enter string: ")
 target = input("Enter character to find from the end: ")
+
+position = -1
+
+for i in range(len(text) - 1, -1, -1):
+    if text[i] == target:
+        position = i
+        break
