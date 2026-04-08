3f5cc557d6dd55811a598fce67df559b76cfb2a4
jenamndz <jenamndz18@gmail.com>
2026-04-08 18:23:51 +0800

    determines which is the smaller and larger number

--- print_range_between.py
+++ print_range_between.py
@@ -1,2 +1,5 @@
 first_number = int(input("Enter the first number: "))
 second_number = int(input("Enter the second number: "))
+
+start = min(first_number, second_number) + 1
+end = max(first_number, second_number)
