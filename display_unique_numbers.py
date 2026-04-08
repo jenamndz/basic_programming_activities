9a3e13202b975829202732cfa44a68c7ae6703e0
jenamndz <jenamndz18@gmail.com>
2026-04-08 18:35:58 +0800

    prints all unique numbers

--- display_unique_numbers.py
+++ display_unique_numbers.py
@@ -3,3 +3,9 @@ numbers = []
 for i in range(10):
     num = int(input(f"Enter number {i+1}: "))
     numbers.append(num)
+
+print("\nNumbers without duplicates:")
+
+for n in numbers:
+    if numbers.count(n) == 1:
+        print(n)
