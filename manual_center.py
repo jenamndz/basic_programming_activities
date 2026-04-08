6297459253db697763cb88488b7a06862cd488f7
jenamndz <jenamndz18@gmail.com>
2026-04-09 03:21:55 +0800

    adds space to the beginning until it reaches the length and adds spaces in the end

--- manual_center.py
+++ manual_center.py
@@ -1,2 +1,10 @@
 text = input("Enter string: ")
 total_length = int(input("Enter total characters: "))
+
+while len(text) < total_length:
+    text = " " + text
+
+    if len(text) == total_length:
+        break
+
+    text = text + " "
