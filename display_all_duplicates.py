3190439e2dc71addd2b888cd3838e43cc1263e90
jenamndz <jenamndz18@gmail.com>
2026-04-08 23:26:15 +0800

    checks if the inputted numbers is entered not just once and stores it in a list

--- display_all_duplicates.py
+++ display_all_duplicates.py
@@ -3,3 +3,10 @@ numbers = []
 for i in range (10):
     num = int(input(f"Enter the number {i+1} : "))
     numbers.append(num)
+
+duplicate_numbers = []
+
+for n in numbers:
+    if numbers.count(n) > 1:
+        duplicate_numbers.append(n)
+
