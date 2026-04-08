b314da53d9b5742e7d0a92c246406218b79b49c7
jenamndz <jenamndz18@gmail.com>
2026-04-08 19:26:45 +0800

    asks the user to input a number and stops the program when the user entered a non number

---
+++ sort_inputs_until_invalid.py
@@ -0,0 +1,11 @@
+numbers = []
+
+print("Enter numbers (Enter any letter or symbol to stop):")
+
+while True:
+    user_input = input("Enter a number: ")
+    try:
+        number = float(user_input)
+        numbers.append(number)
+    except ValueError:
+        break
