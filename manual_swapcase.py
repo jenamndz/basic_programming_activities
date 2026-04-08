8c8ce566c955d20cd14976bb3f5b07f6cecfd630
jenamndz <jenamndz18@gmail.com>
2026-04-09 03:52:34 +0800

    finds the characters from the entered text in the upper list and pull the same letter from the lower list, and does the same for lower list

--- manual_swapcase.py
+++ manual_swapcase.py
@@ -3,3 +3,11 @@ text = input("Enter string: ")
 UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 lower = "abcdefghijklmnopqrstuvwxyz"
 result = ""
+
+for char in text:
+    if char in UPPER:
+        index = UPPER.find(char)
+        result += lower[index]
+    elif char in lower:
+        index = lower.find(char)
+        result += UPPER[index]
