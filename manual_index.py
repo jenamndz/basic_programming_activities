3850ecacc20ad9b80d42c990ff5105005fc40f3d
jenamndz <jenamndz18@gmail.com>
2026-04-09 05:26:41 +0800

    looks for the target character and stops when found

--- manual_index.py
+++ manual_index.py
@@ -1,2 +1,9 @@
 text = input("Enter string: ")
 target = input("Enter character to find: ")
+
+position = -1
+
+for i in range(len(text)):
+    if text[i] == target:
+        position = i
+        break
